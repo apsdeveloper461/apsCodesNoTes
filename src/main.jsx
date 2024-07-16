2d69b9c43ea391b5b2b32a5851891400d778ef0a
Mehboob Alam(A.D) <mehboobalam@gmail.com>
2024-07-16 16:39:19 +0500

    adding ga4

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
-import { ThemeProvider, ThemeContext } from './Context/ThemeContext'
+import { ThemeProvider } from './Context/ThemeContext'
 import App from './App.jsx'
-import {  Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import './index.css'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-5Q4WC6TDYB");
+
+// Send pageview with a custom path
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
